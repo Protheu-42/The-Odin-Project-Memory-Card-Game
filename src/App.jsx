6c9f7018b1600f2edaa7578d2c387b9248bcb36b
Protheu <everaldo.sobrinho42@gmail.com>
2026-06-11 16:54:36 -0300

    feat: pokemonApi create

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,24 @@ import { useEffect, useState } from "react";
 import fetchAllPokemon from "./fetchPokemonAPI";
 
 function App() {
-  const [pokemon, setPokemon] = useState();
+  const [pokemon, setPokemon] = useState([]);
+  let fetchedPokemon = Array.isArray(pokemon);
 
   useEffect(() => {
-    fetchAllPokemon().then((data) => setPokemon(data));
+    fetchAllPokemon().then((data) => setPokemon([...data]));
   }, []);
 
-  return <img src={pokemon} alt="" />;
+  if (fetchedPokemon) {
+    return (
+      <>
+        {pokemon.map((eachPokemon) => {
+          return <img src={eachPokemon} alt="" />;
+        })}
+      </>
+    );
+  } else {
+    return <h1>Loading</h1>;
+  }
 }
 
-// make this a ARRAY
-
 export default App;
-
-/*   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon/ditto")
-      .then((response) => response.json())
-      .then((response) => {
-        console.log(response);
-        setPokemon((prev) => [
-          ...prev,
-          response.sprites.other["official-artwork"].front_default,
-        ]);
-      });
-  }, []); */
--- src/fetchPokemonAPI.js
+++ src/fetchPokemonAPI.js
@@ -1,16 +1,43 @@
-const fetchPokemonApi = (name) =>
-  fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+const pokemonNames = ["ditto", "eevee"];
+let pokemonArray = [];
+
+const fetchPokemonApi = (pokemonName) =>
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
     .then((response) => response.json())
     .then((response) => {
-      console.log(response);
       return response.sprites.other["official-artwork"].front_default;
     });
-
-let array = [];
-
+/* 
 const fetchAllPokemon = () =>
   fetchPokemonApi("ditto").then((data) => {
-    array.push(data);
-    return array;
+    if (pokemonArray.includes(data)) {
+      return pokemonArray;
+    } else {
+      pokemonArray.push(data);
+      return pokemonArray;
+    }
   });
+ */
+
+async function fetchAllPokemon() {
+  try {
+    const fetchPromises = pokemonNames.map((pokemonName) =>
+      fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`),
+    );
+    const responses = await Promise.all(fetchPromises);
+    const data = await Promise.all(
+      responses.map((res) => {
+        return res.json();
+      }),
+    );
+    const sprite = data.map(
+      (cel) => cel.sprites.other["official-artwork"].front_default,
+    );
+
+    return sprite;
+  } catch (error) {
+    console.error("fetch failed" + error);
+  }
+}
+
 export default fetchAllPokemon;
