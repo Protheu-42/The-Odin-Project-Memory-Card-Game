e18df5e5117ed0c329ef9381be7e6f6eb022a018
Protheu <everaldo.sobrinho42@gmail.com>
2026-06-14 17:53:02 -0300

    feat: scores, arrayOfChoices randomize create

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,46 @@
 import { useEffect, useState } from "react";
 import fetchAllPokemon from "./fetchPokemonAPI";
 
-function Card({ url }) {
+function Card({ url, clickEvent }) {
   return (
     <li>
-      <img src={url} />
+      <img src={url} onClick={() => clickEvent()} />
     </li>
   );
 }
 
 function App() {
   const [pokemon, setPokemon] = useState([]);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [arrayOfChoices, setArrayOfChoices] = useState([]);
+
   let fetchedPokemon = Array.isArray(pokemon);
+
+  function randomizingPokemonArray() {
+    setPokemon(pokemon.sort(() => Math.random() - 0.5));
+  }
+
+  function handleClickEvent(data) {
+    randomizingPokemonArray();
+    console.log(pokemon);
+    if (arrayOfChoices.includes(data)) {
+      if (currentScore > bestScore) {
+        setBestScore(currentScore);
+        setCurrentScore(0);
+        setArrayOfChoices([]);
+      } else {
+        setCurrentScore(0);
+        setArrayOfChoices([]);
+      }
+    } else {
+      setArrayOfChoices((prevArray) => [...prevArray, data]);
+      setCurrentScore((prevScore) => prevScore + 1);
+      if (currentScore > bestScore) {
+        setBestScore(currentScore);
+      }
+    }
+  }
 
   useEffect(() => {
     fetchAllPokemon().then((data) => setPokemon([...data]));
@@ -20,9 +49,17 @@ function App() {
   if (fetchedPokemon) {
     return (
       <>
+        <h2>Current Score: {currentScore}</h2>
+        <h2>Best Score: {bestScore}</h2>
         <ul>
           {pokemon.map((eachPokemon) => {
-            return <Card key={eachPokemon} url={eachPokemon} />;
+            return (
+              <Card
+                key={eachPokemon}
+                url={eachPokemon}
+                clickEvent={() => handleClickEvent(eachPokemon)}
+              />
+            );
           })}
         </ul>
       </>
